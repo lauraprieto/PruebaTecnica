3e93481af4b6643bc9bb451d59e7f526ce4459e4
Laura Prieto <Laura.prieto@mocionsoft.com>
2020-06-20 23:07:58 -0500

    agregando filtro de contenido

--- src/Components/Body/content.jsx
+++ src/Components/Body/content.jsx
@@ -8,70 +8,55 @@ class Content extends Component {
     render() {
         return (
             <>
-                <div className="container shadow-lg p-3 mb-5 bg-white rounded">
-                    <div className="filter">
+                <div className="container shadow-lg p-5 mb-5 bg-white rounded">
+                    <div className="filter mb-5">
+                        <form>
+                            <div class="form-row align-items-center ">
+                                <div class="col-auto my-1 ">
+                                    <select class="custom-select mr-sm-2 shadow bg-white" id="inlineFormCustomSelect">
+                                        <option selected>Ordenar por</option>
+                                        <option value="1">Más recientes</option>
+                                        <option value="2">Menos recientes</option>
+                                        <option value="3">Tipos de recursos</option>
+                                    </select>
+                                </div>
 
+                                <div class="col-auto my-1">
+                                    <button type="submit" class="btn btn-secondary text-dark shadow bg-white rounded">Filtrar</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
 
-                    <div className="containerInformation">
+                    <hr/>
+
+                    <div className="containerInformation mt-5">
                         <div class="card-columns">
-                            <div class="card">
+                            <div class="card shadow bg-white rounded">
                                 <img src={NewOne} class="card-img-top" alt="Noticia 1"/>
                                 <div class="card-body">
                                     <h5 class="card-title">Card title that wraps to a new line</h5>
                                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 </div>
                             </div>
-                            <div class="card">
-                                <blockquote class="blockquote mb-0 card-body">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                <footer class="blockquote-footer">
-                                    <small class="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                    </small>
-                                </footer>
-                                </blockquote>
+                            <div class="card shadow bg-white rounded">
+                                <img src={NewOne} class="card-img-top" alt="Noticia 1"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title that wraps to a new line</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                </div>
                             </div>
-                            <div class="card">
-                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                            
+                            <div class="card shadow bg-white rounded">
                                 <div class="card-body">
                                 <h5 class="card-title">Card title</h5>
                                 <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
-                            <div class="card bg-primary text-white text-center p-3">
-                                <blockquote class="blockquote mb-0">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
-                                <footer class="blockquote-footer text-white">
-                                    <small>
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                    </small>
-                                </footer>
-                                </blockquote>
+                            <div class="card shadow bg-white rounded">
+                                <img src={NewTwo} class="card-img-top" alt="noticia 2"/>
                             </div>
-
-                            <div class="card p-3 text-right">
-                                <blockquote class="blockquote mb-0">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                <footer class="blockquote-footer">
-                                    <small class="text-muted">
-                                    Someone famous in <cite title="Source Title">Source Title</cite>
-                                    </small>
-                                </footer>
-                                </blockquote>
-                            </div>
-
-                            <div class="card">
-                                {/* <img src="..." class="card-img-top" alt="..."> */}
-                            </div>
-                            {/* <div class="card">
-                                <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is another card with title and supporting text below. This card has some additional content to make it slightly taller overall.</p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                                </div>
-                            </div> */}
                         </div>
                     </div>
                 </div>
